0d64c81b3a61d7d49fbed0189ea07b3a222fd6c7
mohit <mohittpattel@gmail.com>
2021-05-06 14:29:22 +0530

    testing.cpp program created

---
+++ tesing.cpp
@@ -0,0 +1,12 @@
+// C++ program to print Hello without curly brases
+#include<iostream>
+using namespace std;
+int main()
+{
+    if(cout<<"Hello World Using IF\n")
+
+    while(cout<<"Hello World Using While\n")
+        break;
+
+    return 0;
+}
